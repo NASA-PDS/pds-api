9381b907fb116f67a93ffb36a9d085442a19f381
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2023-01-24 12:57:20 -0800

    upgrade for build 13.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'B12.1',
+    VERSION: 'B13.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
